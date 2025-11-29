f80f4dab2c9f340fb3776c028e4b2672ae976ecb
unknownuser487 <rahulprasanth487@gmail.com>
2025-11-29 13:49:46 +0530

    Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// App.tsx
+import React from "react";
+
+const App: React.FC = () => {
+
+  return (
+    <div className="min-h-screen bg-slate-950 text-white">
+      
+    </div>
+  );
+};
+
+export default App;
