311de349f84c8480b6b4910d6023acb5ffc7aa33
RAHUL PRASANTH D <75294742+rahulprasanth487@users.noreply.github.com>
2026-01-24 10:33:13 +0530

    Update API URL to use environment variable

--- src/components/CoupleDetails/CoupleDetails.tsx
+++ src/components/CoupleDetails/CoupleDetails.tsx
@@ -115,7 +115,7 @@ const CoupleDetails: React.FC<CoupleDetailsProps> = (props) => {
 
         console.log("Sending data to backend for matching...");
         try {
-            const request = await fetch(`/api/match`, {
+            const request = await fetch(import.meta.env.VITE_BACKEND_API_URL + `/api/match`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ boy, girl }),
